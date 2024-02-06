35678b78cbc70db1d56ce581887c1afa6e7d3248
ohmy04180 <ohmy0418@mobigen.com>
2024-02-06 16:46:03 +0900

    [FAB2023-512] feat: Pagination 퍼블리싱 구성

--- component-ui/components/Pagination.vue
+++ component-ui/components/Pagination.vue
@@ -1,6 +1,24 @@
 <template>
-	<div>
-		This is Pagination Component
+	<div class="pagination">
+		<button class="pagination-move-button" type="button">
+			<<
+		</button>
+		<button class="pagination-move-button" type="button">
+			<
+		</button>
+		<ul class="pagination-list">
+			<li class="pagination-item">
+				<button type="button" class="pagination-button">
+					1
+				</button>
+			</li>
+		</ul>
+		<button class="pagination-move-button" type="button">
+			>
+		</button>
+		<button class="pagination-move-button" type="button">
+			>>
+		</button>
 	</div>
 </template>
 
@@ -9,5 +27,22 @@ export default {}
 </script>
 
 <style scoped>
+.pagination, .pagination-move-button, .pagination-list {
+	display: flex;
+}
 
+.pagination, .pagination-list {
+	gap: 16px;
+}
+
+.pagination-move-button, .pagination-button {
+	dislay: flex;
+	align-items: center;
+	justify-content: center;
+	width: 50px;
+	height: 50px;
+	background-color: #fff;
+	border-color: #333;
+	font-size: 20px;
+}
 </style>
--- component-ui/pages/index.vue
+++ component-ui/pages/index.vue
@@ -11,3 +11,25 @@ export default {
 	components: {Pagination}
 }
 </script>
+<style>
+html {
+	width: 100%;
+	height: 100vh;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+}
+
+html, body, div, span, ol, ul, li {
+	margin: 0;
+	padding: 0;
+	border: 0;
+	font-size: 100%;
+	font: inherit;
+	vertical-align: baseline;
+}
+
+ol, ul {
+	list-style: none;
+}
+</style>
