d13778712d99515282cb8520a9656299d02fdb76
ohmy04180 <ohmy0418@mobigen.com>
2024-02-07 16:52:33 +0900

    [FAB2023-512] feat: Pagination 폴더 구조 변경, 선언 방식 수정

--- component-ui/components/Pagination.vue
+++ component-ui/components/Pagination.vue
@@ -1,29 +1,105 @@
 <template>
+	<h1>{{ title }}</h1>
+	<div></div>
 	<div class="pagination">
-		<button class="pagination-move-button" type="button">
-			<<
-		</button>
-		<button class="pagination-move-button" type="button">
+		<button class="pagination-move-button" type="button" @click="movePrevPage" :disabled="isPrevDisabled">
 			<
 		</button>
 		<ul class="pagination-list">
-			<li class="pagination-item">
-				<button type="button" class="pagination-button">
-					1
+			<li class="pagination-item" v-for="item in newData" :key="item.number">
+				<button type="button" class="pagination-button" :class="item.number == currentPage ? 'is-active':''"
+								@click="movePage(item)">
+					{{ item.number }}
 				</button>
 			</li>
 		</ul>
-		<button class="pagination-move-button" type="button">
+		<button class="pagination-move-button" type="button" @click="moveNextPage" :disabled="isNextDisabled">
 			>
-		</button>
-		<button class="pagination-move-button" type="button">
-			>>
 		</button>
 	</div>
 </template>
 
-<script>
-export default {}
+<script lang="ts">
+import {ref} from 'vue';
+
+export default defineComponent({
+	props: ['data'],
+	setup(props, {emit}) {
+
+		//	** DATA
+		// 총 게시글 수
+		const totalCount: object[] = props.data;
+		// 보여질 숫자 버튼 개수 (10개씩 보기, 20개씩 보기)
+		const perPage = ref(10);
+		// 현재 페이지 (Default: 1)
+		let currentPage = ref(1);
+		// 페이지네이션에 몇 개의 페이지가 보이게 할 것인지
+		const pageSize = ref(10);
+		// disabled 체크
+		let isPrevDisabled = false;
+		let isNextDisabled = false;
+
+		let newData: object[] = [];
+
+		if(props.data){
+			newData = props.data.slice(props.data[0], perPage.value);
+			isPrevDisabled = true;
+		}
+
+		//	** COMPUTED
+		// data에서 보여질 페이지 개수에 맞춰서 페이지에 출력한다.
+		let slicedData = computed(() => {
+			// let newData: object[] = [];
+			// newData = props.data.slice(props.data[0], perPage.value);
+			// return newData;
+			return ''
+		})
+
+		//	** METHODS
+		// 숫자 버튼 클릭시 해당하는 데이터 목록을 출력한다.
+		const movePage = (item) => {
+			currentPage = item.number;
+			console.log('클릭한 숫자: ', item.number)
+			console.log('현재 선택된 숫자: ', currentPage);
+		}
+
+		// 전체 개수 / 보여질 페이지 개수
+		let totalPage = Math.ceil(totalCount.length / pageSize.value);
+		console.log('totalPage', totalPage)
+
+		function movePrevPage() {
+			console.log('다음 페이지로 이동');
+		}
+
+		function moveNextPage() {
+			console.log('이전 페이지로 이동');
+		}
+
+		// TODO: 버릴것 임의 코드
+		const title = ref('Pagination Component');
+
+		const reversedMessage = computed(() => {
+			return title.value.split('').reverse().join('');
+		})
+
+		return {
+			title,
+			reversedMessage,
+			//
+			isPrevDisabled,
+			isNextDisabled,
+			movePage,
+			movePrevPage,
+			totalCount,
+			perPage,
+			currentPage,
+			pageSize,
+			slicedData,
+			newData
+		}
+	},
+});
+
 </script>
 
 <style scoped>
@@ -42,7 +118,19 @@ export default {}
 	width: 50px;
 	height: 50px;
 	background-color: #fff;
-	border-color: #333;
+	border: none;
+	border-radius: 4px;
 	font-size: 20px;
+	cursor: pointer;
+}
+
+.pagination-button.is-active {
+	background-color: royalblue;
+	color: #fff;
+}
+
+button:disabled {
+	background-color: lightgray;
+	cursor: not-allowed;
 }
 </style>
--- component-ui/pages/index.vue
+++ component-ui/pages/index.vue
@@ -1,15 +1,35 @@
 <template>
 	<div>
-		<Pagination></Pagination>
+		<div></div>
+		<Pagination :data="sampleData"></Pagination>
 	</div>
 </template>
 
-<script>
-import Pagination from "../components/Pagination";
+<script lang="ts">
+import {ref} from 'vue';
 
-export default {
-	components: {Pagination}
-}
+export default defineComponent({
+	setup() {
+		const sampleData = [
+			{number: 1, value: 'title1'},
+			{number: 2, value: 'title2'},
+			{number: 3, value: 'title3'},
+			{number: 4, value: 'title4'},
+			{number: 5, value: 'title5'},
+			{number: 6, value: 'title6'},
+			{number: 7, value: 'title7'},
+			{number: 8, value: 'title8'},
+			{number: 9, value: 'title9'},
+			{number: 10, value: 'title10'},
+			{number: 11, value: 'title11'},
+			{number: 12, value: 'title12'},
+			{number: 13, value: 'title13'},
+			{number: 114, value: 'title14'},
+		]
+		return {sampleData}
+	}
+})
+
 </script>
 <style>
 html {
@@ -27,6 +47,12 @@ html, body, div, span, ol, ul, li {
 	font-size: 100%;
 	font: inherit;
 	vertical-align: baseline;
+}
+
+h1 {
+	display: flex;
+	align-items: center;
+	justify-content: center;
 }
 
 ol, ul {
