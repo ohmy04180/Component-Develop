eb7f79298b66a87ecddebc22716bb351fd3aeedf
ohmy04180 <ohmy0418@mobigen.com>
2024-02-07 20:28:30 +0900

    [FAB2023-512] feat: Pagination 기능개발 진행-숫자 버튼 클릭시 활성화, 첫 번째 위치일 경우 이전버튼 비활성화

--- component-ui/components/Pagination.vue
+++ component-ui/components/Pagination.vue
@@ -1,136 +1,162 @@
 <template>
-	<h1>{{ title }}</h1>
-	<div></div>
-	<div class="pagination">
-		<button class="pagination-move-button" type="button" @click="movePrevPage" :disabled="isPrevDisabled">
-			<
-		</button>
-		<ul class="pagination-list">
-			<li class="pagination-item" v-for="item in newData" :key="item.number">
-				<button type="button" class="pagination-button" :class="item.number == currentPage ? 'is-active':''"
-								@click="movePage(item)">
-					{{ item.number }}
-				</button>
-			</li>
-		</ul>
-		<button class="pagination-move-button" type="button" @click="moveNextPage" :disabled="isNextDisabled">
-			>
-		</button>
-	</div>
+  <h1>Pagination</h1>
+
+  <div class="pagination">
+    <button
+      class="pagination-move-button"
+      type="button"
+      @click="movePrevPage"
+      :disabled="isPrevDisabled"
+    >
+      <
+    </button>
+    <ul class="pagination-list">
+      <li
+        class="pagination-item"
+        v-for="item in slicedPaginationData"
+        :key="item"
+      >
+        <button
+          type="button"
+          class="pagination-button"
+          @click="movePage(item)"
+          :class="item === currentPage ? 'is-active' : ''"
+        >
+          {{ item }}
+        </button>
+      </li>
+    </ul>
+    <button
+      class="pagination-move-button"
+      type="button"
+      @click="moveNextPage"
+      :disabled="isNextDisabled"
+    >
+      >
+    </button>
+  </div>
 </template>
 
 <script lang="ts">
-import {ref} from 'vue';
+import { ref, defineComponent, computed } from "vue";
 
 export default defineComponent({
-	props: ['data'],
-	setup(props, {emit}) {
+  props: ["paginationData", "postData"],
+  setup(props, { emit }) {
+    //	** DATA
+    // 총 게시글 수
+    const totalCount: object[] = props.postData.length;
+    // 보여질 숫자 버튼 개수 (10개씩 보기, 20개씩 보기)
+    const perPage = ref(10);
+    // 현재 페이지 (Default: 1)
+    let currentPage = ref(1);
+    // 페이지네이션에 몇 개의 페이지가 보이게 할 것인지
+    const pageSize: number = 10;
+    // disabled 체크
+    let isPrevDisabled = ref(false);
+    let isNextDisabled = ref(false);
 
-		//	** DATA
-		// 총 게시글 수
-		const totalCount: object[] = props.data;
-		// 보여질 숫자 버튼 개수 (10개씩 보기, 20개씩 보기)
-		const perPage = ref(10);
-		// 현재 페이지 (Default: 1)
-		let currentPage = ref(1);
-		// 페이지네이션에 몇 개의 페이지가 보이게 할 것인지
-		const pageSize = ref(10);
-		// disabled 체크
-		let isPrevDisabled = false;
-		let isNextDisabled = false;
+    // ** COMPUTED
+    // 처음 진입 시 보여질 페이지네이션 개수 출력
+    const slicedPaginationData = computed(() => {
+      return props.paginationData.slice(0, perPage.value);
+    });
 
-		let newData: object[] = [];
+    //	** METHODS
 
-		if(props.data){
-			newData = props.data.slice(props.data[0], perPage.value);
-			isPrevDisabled = true;
-		}
+    // TODO: [개발]
+    // let totalPage = Math.ceil(totalCount.length / pageSize.value);
 
-		//	** COMPUTED
-		// data에서 보여질 페이지 개수에 맞춰서 페이지에 출력한다.
-		let slicedData = computed(() => {
-			// let newData: object[] = [];
-			// newData = props.data.slice(props.data[0], perPage.value);
-			// return newData;
-			return ''
-		})
+    // 숫자 버튼 클릭시 해당하는 데이터 목록을 출력한다.
+    function movePage(item: number) {
+      // 이전 / 다음으로 버튼 비활성화 체크
+      checkPrevDisabled(item);
+      checkNextDisabled(item);
 
-		//	** METHODS
-		// 숫자 버튼 클릭시 해당하는 데이터 목록을 출력한다.
-		const movePage = (item) => {
-			currentPage = item.number;
-			console.log('클릭한 숫자: ', item.number)
-			console.log('현재 선택된 숫자: ', currentPage);
-		}
+      // 현재 페이지 상태 설정
+      isSelectedClass(item);
+    }
 
-		// 전체 개수 / 보여질 페이지 개수
-		let totalPage = Math.ceil(totalCount.length / pageSize.value);
-		console.log('totalPage', totalPage)
+    // TODO: [개발] currentPage 변화 감지, emit 발생
+    function isSelectedClass(item: number) {
+      currentPage.value = item;
 
-		function movePrevPage() {
-			console.log('다음 페이지로 이동');
-		}
+      // test
+      // console.log("currentPage.value", currentPage.value);
+    }
 
-		function moveNextPage() {
-			console.log('이전 페이지로 이동');
-		}
+    // TODO: [개발] 1번째 순서에 도달했을 경우, 이전 버튼 비활성화
+    function checkPrevDisabled(item: number): void {
+      isPrevDisabled.value = item === props.paginationData[0] ? true : false;
+    }
 
-		// TODO: 버릴것 임의 코드
-		const title = ref('Pagination Component');
+    // TODO: [개발] 마지막 순서에 도달했을 경우, 다음 버튼 비활성화
+    function checkNextDisabled(item: number): void {
+      isNextDisabled.value =
+        item == props.paginationData[props.paginationData.length - 1]
+          ? true
+          : false;
+    }
 
-		const reversedMessage = computed(() => {
-			return title.value.split('').reverse().join('');
-		})
+    function movePrevPage() {
+      console.log("다음 페이지로 이동");
+    }
 
-		return {
-			title,
-			reversedMessage,
-			//
-			isPrevDisabled,
-			isNextDisabled,
-			movePage,
-			movePrevPage,
-			totalCount,
-			perPage,
-			currentPage,
-			pageSize,
-			slicedData,
-			newData
-		}
-	},
+    function moveNextPage() {
+      console.log("이전 페이지로 이동");
+    }
+
+    return {
+      slicedPaginationData,
+      isPrevDisabled,
+      isNextDisabled,
+      movePage,
+      movePrevPage,
+      totalCount,
+      perPage,
+      currentPage,
+      pageSize,
+      isSelectedClass,
+      checkPrevDisabled,
+      checkNextDisabled
+    };
+  }
 });
-
 </script>
 
 <style scoped>
-.pagination, .pagination-move-button, .pagination-list {
-	display: flex;
+.pagination,
+.pagination-move-button,
+.pagination-list {
+  display: flex;
 }
 
-.pagination, .pagination-list {
-	gap: 16px;
+.pagination,
+.pagination-list {
+  gap: 16px;
 }
 
-.pagination-move-button, .pagination-button {
-	dislay: flex;
-	align-items: center;
-	justify-content: center;
-	width: 50px;
-	height: 50px;
-	background-color: #fff;
-	border: none;
-	border-radius: 4px;
-	font-size: 20px;
-	cursor: pointer;
+.pagination-move-button,
+.pagination-button {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 50px;
+  height: 50px;
+  background-color: #fff;
+  border: none;
+  border-radius: 4px;
+  font-size: 20px;
+  cursor: pointer;
 }
 
 .pagination-button.is-active {
-	background-color: royalblue;
-	color: #fff;
+  background-color: royalblue;
+  color: #fff;
 }
 
 button:disabled {
-	background-color: lightgray;
-	cursor: not-allowed;
+  background-color: lightgray;
+  cursor: not-allowed;
 }
 </style>
--- component-ui/pages/index.vue
+++ component-ui/pages/index.vue
@@ -1,61 +1,70 @@
 <template>
-	<div>
-		<div></div>
-		<Pagination :data="sampleData"></Pagination>
-	</div>
+  <div>
+    <div></div>
+    <Pagination
+      :paginationData="paginationList"
+      :postData="postList"
+    ></Pagination>
+  </div>
 </template>
 
 <script lang="ts">
-import {ref} from 'vue';
+import { ref, defineComponent } from "vue";
 
 export default defineComponent({
-	setup() {
-		const sampleData = [
-			{number: 1, value: 'title1'},
-			{number: 2, value: 'title2'},
-			{number: 3, value: 'title3'},
-			{number: 4, value: 'title4'},
-			{number: 5, value: 'title5'},
-			{number: 6, value: 'title6'},
-			{number: 7, value: 'title7'},
-			{number: 8, value: 'title8'},
-			{number: 9, value: 'title9'},
-			{number: 10, value: 'title10'},
-			{number: 11, value: 'title11'},
-			{number: 12, value: 'title12'},
-			{number: 13, value: 'title13'},
-			{number: 114, value: 'title14'},
-		]
-		return {sampleData}
-	}
-})
+  setup() {
+    const paginationList: Array<number> = [
+      1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16
+    ];
 
+    const postList: object[] = [
+      { title: "Title", postContent: "This is Content" },
+      { title: "Title", postContent: "This is Content" },
+      { title: "Title", postContent: "This is Content" },
+      { title: "Title", postContent: "This is Content" },
+      { title: "Title", postContent: "This is Content" },
+      { title: "Title", postContent: "This is Content" },
+      { title: "Title", postContent: "This is Content" },
+      { title: "Title", postContent: "This is Content" },
+      { title: "Title", postContent: "This is Content" },
+      { title: "Title", postContent: "This is Content" }
+    ];
+    return { paginationList, postList };
+  }
+});
 </script>
 <style>
 html {
-	width: 100%;
-	height: 100vh;
-	display: flex;
-	align-items: center;
-	justify-content: center;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 
-html, body, div, span, ol, ul, li {
-	margin: 0;
-	padding: 0;
-	border: 0;
-	font-size: 100%;
-	font: inherit;
-	vertical-align: baseline;
+html,
+body,
+div,
+span,
+ol,
+ul,
+li {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  font-size: 100%;
+  font: inherit;
+  vertical-align: baseline;
 }
 
 h1 {
-	display: flex;
-	align-items: center;
-	justify-content: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 
-ol, ul {
-	list-style: none;
+ol,
+ul {
+  list-style: none;
 }
 </style>
