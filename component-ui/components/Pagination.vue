e5edc4ca836d9c42f4c234c515153be465419ce9
ohmy04180 <ohmy0418@mobigen.com>
2024-02-13 13:10:32 +0900

    [FAB2023-512] feat: Pagination 1차 개발 완료

--- component-ui/components/Pagination.vue
+++ component-ui/components/Pagination.vue
@@ -1,116 +1,127 @@
 <template>
-  <h1>Pagination</h1>
-
-  <div class="pagination">
-    <button
-      class="pagination-move-button"
-      type="button"
-      @click="movePrevPage"
-      :disabled="isPrevDisabled"
-    >
-      <
-    </button>
-    <ul class="pagination-list">
-      <li class="pagination-item" v-for="item in paginationList" :key="item">
-        <button
-          type="button"
-          class="pagination-button"
-          @click="movePage(item)"
-          :class="item === currentPage ? 'is-active' : ''"
-        >
-          {{ item }}
-        </button>
-      </li>
-    </ul>
-    <button
-      class="pagination-move-button"
-      type="button"
-      @click="moveNextPage"
-      :disabled="isNextDisabled"
-    >
-      >
-    </button>
-  </div>
+	<div class="pagination">
+		<button
+			class="pagination-move-button"
+			type="button"
+			@click="movePrevPage"
+			:disabled="isPrevDisabled"
+		>
+			<
+		</button>
+		<ul class="pagination-list">
+			<li class="pagination-item" v-for="(item, index) in paginationList" :key="index">
+				<button
+					type="button"
+					class="pagination-button"
+					@click="movePage(item)"
+					:class="item === currentPage ? 'is-active' : ''"
+				>
+					{{ item }}
+				</button>
+			</li>
+		</ul>
+		<button
+			class="pagination-move-button"
+			type="button"
+			@click="moveNextPage"
+			:disabled="isNextDisabled"
+		>
+			>
+		</button>
+	</div>
 </template>
 
 <script lang="ts">
-import { ref, defineComponent } from "vue";
+import {ref, defineComponent} from "vue";
 
 export default defineComponent({
-  props: ["paginationList", "paginationCount"],
-  setup(props, { emit }) {
-    //	DATA
-    // 현재 페이지 (Default 값 = 1)
-    let currentPage = ref(1);
-    // disabled 체크
-    let isPrevDisabled = ref(false);
-    let isNextDisabled = ref(false);
+	props: ["perPage", "totalCount"],
+	setup(props, {emit}) {
+		//	DATA
+		// 총 게시글 수 / 보여질 게시글 수
+		const paginationCount: number = Math.ceil(props.totalCount / props.perPage);
+		// 현재 페이지 (Default = 1)
+		let currentPage = ref(1);
+		// 페이지네이션 목록화
+		let paginationList = ref([]);
+		// 이전/다음 버튼 비활성화 체크
+		let isPrevDisabled = ref(true);
+		let isNextDisabled = paginationCount !== 1 ? ref(false) : ref(true)
 
-    //	** METHODS
-    // 숫자 버튼 클릭시 해당하는 데이터 목록을 출력
-    function movePage(item: number) {
-      checkPrevDisabled(item);
-      checkNextDisabled(item);
+		// CREATED
+		for (let i: number = 1; i <= paginationCount; i++) {
+			paginationList.value.push(i as never);
+		}
 
-      isSelectedClass(item);
-    }
+		//	METHODS
+		// 클릭한 버튼 감지
+		function movePage(item: number): void {
+			checkPrevDisabled(item);
+			checkNextDisabled(item);
+			isSelectedClass(item);
+			emit("checkCurrentPage", item);
+		}
 
-    // 클릭한 선택 값을 currentPage에 부여
-    function isSelectedClass(item: number) {
-      currentPage.value = item;
-    }
+		// 클릭한 선택 값을 currentPage에 부여
+		function isSelectedClass(item: number): void {
+			currentPage.value = item;
+		}
 
-    // 첫번째 순서에 도달했을 경우, 이전 버튼 비활성화
-    function checkPrevDisabled(item: number): void {
-      isPrevDisabled.value = item === props.paginationList[0] ? true : false;
-    }
+		// 첫번째 순서에 도달했을 경우 이전 버튼 비활성화
+		function checkPrevDisabled(item: number): void {
+			isPrevDisabled.value = item === paginationList.value[0] ? true : false;
+		}
 
-    // 마지막 순서에 도달했을 경우, 다음 버튼 비활성화
-    function checkNextDisabled(item: number): void {
-      isNextDisabled.value =
-        item == props.paginationList[props.paginationList.length - 1]
-          ? true
-          : false;
-    }
+		// 마지막 순서에 도달했을 경우 다음 버튼 비활성화
+		function checkNextDisabled(item: number): void {
+			isNextDisabled.value =
+				item == paginationList.value[paginationList.value.length - 1]
+					? true
+					: false;
+		}
 
-    // 이전 버튼 클릭 시 currentPage 값 변경 + 비활성화 설정
-    function movePrevPage() {
-      if (currentPage.value > 0) {
-        currentPage.value = currentPage.value - 1;
+		// 이전 버튼 클릭 시 currentPage 값 변경 + 비활성화 체크
+		function movePrevPage(): void {
+			if (currentPage.value > 0) {
+				currentPage.value = currentPage.value - 1;
 
-        isPrevDisabled.value = currentPage.value == 1 ? true : false;
-      } else {
-        return;
-      }
+				isPrevDisabled.value = currentPage.value == 1 ? true : false;
+			} else {
+				return;
+			}
 
-      isNextDisabled.value =
-        currentPage.value < props.paginationCount ? false : true;
-    }
+			isNextDisabled.value =
+				currentPage.value < paginationCount ? false : true;
 
-    // 다음 버튼 클릭 시 currentPage 값 변경 + 비활성화 설정
-    function moveNextPage() {
-      if (currentPage.value < props.paginationCount) {
-        currentPage.value = currentPage.value + 1;
+			emit("checkCurrentPage", currentPage.value);
+		}
 
-        isNextDisabled.value =
-          currentPage.value === props.paginationCount ? true : false;
-      } else {
-        return;
-      }
+		// 다음 버튼 클릭 시 currentPage 값 변경 + 비활성화 체크
+		function moveNextPage(): void {
+			if (currentPage.value < paginationCount) {
+				currentPage.value = currentPage.value + 1;
 
-      isPrevDisabled.value = currentPage.value > 0 ? false : true;
-    }
+				isNextDisabled.value =
+					currentPage.value === paginationCount ? true : false;
+			} else {
+				return;
+			}
 
-    return {
-      isPrevDisabled,
-      isNextDisabled,
-      movePage,
-      movePrevPage,
-      moveNextPage,
-      currentPage,
-      isSelectedClass
-    };
-  }
+			isPrevDisabled.value = currentPage.value > 0 ? false : true;
+
+			emit("checkCurrentPage", currentPage.value);
+		}
+
+		return {
+			isPrevDisabled,
+			isNextDisabled,
+			movePage,
+			movePrevPage,
+			moveNextPage,
+			currentPage,
+			paginationList
+		};
+	}
 });
 </script>
 
@@ -118,35 +129,35 @@ export default defineComponent({
 .pagination,
 .pagination-move-button,
 .pagination-list {
-  display: flex;
+	display: flex;
 }
 
 .pagination,
 .pagination-list {
-  gap: 16px;
+	gap: 16px;
 }
 
 .pagination-move-button,
 .pagination-button {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 50px;
-  height: 50px;
-  background-color: #fff;
-  border: none;
-  border-radius: 4px;
-  font-size: 20px;
-  cursor: pointer;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	width: 50px;
+	height: 50px;
+	background-color: #fff;
+	border: none;
+	border-radius: 4px;
+	font-size: 20px;
+	cursor: pointer;
 }
 
 .pagination-button.is-active {
-  background-color: royalblue;
-  color: #fff;
+	background-color: royalblue;
+	color: #fff;
 }
 
 button:disabled {
-  background-color: lightgray;
-  cursor: not-allowed;
+	background-color: lightgray;
+	cursor: not-allowed;
 }
 </style>
--- component-ui/pages/index.vue
+++ component-ui/pages/index.vue
@@ -1,118 +1,111 @@
 <template>
-  <div>
-    <div></div>
-    <Pagination
-      :paginationCount="paginationCount"
-      :paginationList="paginationList"
-    ></Pagination>
-  </div>
+	<div class="group">
+		<h1>Pagination Component</h1>
+		<Pagination
+			:totalCount="totalCount"
+			:perPage="perPage"
+			@checkCurrentPage="checkCurrentPage"
+		></Pagination>
+	</div>
 </template>
 
 <script lang="ts">
-import { ref, defineComponent } from "vue";
+import {defineComponent} from "vue";
 
 export default defineComponent({
-  setup() {
-    // DATA
-    // TODO: [개발] currentPage를 가져와서 게시글 목록 보여주기
-    // 예) 1번 currentPage를 가져왔을 때, 0-9개의 목록을 보여준다.
-    // 게시글 리스트
-    const postList: object[] = [
-      { title: "Title", postContent: "This is Content" },
-      { title: "Title", postContent: "This is Content" },
-      { title: "Title", postContent: "This is Content" },
-      { title: "Title", postContent: "This is Content" },
-      { title: "Title", postContent: "This is Content" },
-      { title: "Title", postContent: "This is Content" },
-      { title: "Title", postContent: "This is Content" },
-      { title: "Title", postContent: "This is Content" },
-      { title: "Title", postContent: "This is Content" },
-      { title: "Title", postContent: "This is Content" },
-      { title: "Title", postContent: "This is Content" },
-      { title: "Title", postContent: "This is Content" },
-      { title: "Title", postContent: "This is Content" },
-      { title: "Title", postContent: "This is Content" },
-      { title: "Title", postContent: "This is Content" },
-      { title: "Title", postContent: "This is Content" },
-      { title: "Title", postContent: "This is Content" },
-      { title: "Title", postContent: "This is Content" },
-      { title: "Title", postContent: "This is Content" },
-      { title: "Title", postContent: "This is Content" },
-      { title: "Title", postContent: "This is Content" },
-      { title: "Title", postContent: "This is Content" },
-      { title: "Title", postContent: "This is Content" },
-      { title: "Title", postContent: "This is Content" },
-      { title: "Title", postContent: "This is Content" },
-      { title: "Title", postContent: "This is Content" },
-      { title: "Title", postContent: "This is Content" },
-      { title: "Title", postContent: "This is Content" },
-      { title: "Title", postContent: "This is Content" },
-      { title: "Title", postContent: "This is Content" },
-      { title: "Title", postContent: "This is Content" },
-      { title: "Title", postContent: "This is Content" },
-      { title: "Title", postContent: "This is Content" },
-      { title: "Title", postContent: "This is Content" },
-      { title: "Title", postContent: "This is Content" },
-      { title: "Title", postContent: "This is Content" },
-      { title: "Title", postContent: "This is Content" },
-      { title: "Title", postContent: "This is Content" },
-      { title: "Title", postContent: "This is Content" },
-      { title: "Title", postContent: "This is Content" },
-      { title: "Title", postContent: "This is Content" },
-      { title: "Title", postContent: "This is Content" },
-      { title: "Title", postContent: "This is Content" },
-      { title: "Title", postContent: "This is Content" },
-      { title: "Title", postContent: "This is Content" },
-      { title: "Title", postContent: "This is Content" },
-      { title: "Title", postContent: "This is Content" },
-      { title: "Title", postContent: "This is Content" },
-      { title: "Title", postContent: "This is Content" },
-      { title: "Title", postContent: "This is Content" },
-      { title: "Title", postContent: "This is Content" },
-      { title: "Title", postContent: "This is Content" },
-      { title: "Title", postContent: "This is Content" },
-      { title: "Title", postContent: "This is Content" },
-      { title: "Title", postContent: "This is Content" },
-      { title: "Title", postContent: "This is Content" },
-      { title: "Title", postContent: "This is Content" },
-      { title: "Title", postContent: "This is Content" },
-      { title: "Title", postContent: "This is Content" },
-      { title: "Title", postContent: "This is Content" },
-      { title: "Title", postContent: "This is Content" },
-      { title: "Title", postContent: "This is Content" }
-    ];
-    // 총 게시글 수
-    const totalCount: number = postList.length;
-    // 보여질 게시글 개수 (10개씩 보기, 20개씩 보기)
-    const perPage: number = 10;
 
-    // 페이지네이션 개수에 맞는 배열을 담을 공간
-    let paginationList = ref([]);
+	setup() {
+		// DATA
+		// 게시글 리스트
+		const postList: object[] = [
+			{title: "Title1", postContent: "This is Content"},
+			{title: "Title2", postContent: "This is Content"},
+			{title: "Title3", postContent: "This is Content"},
+			{title: "Title4", postContent: "This is Content"},
+			{title: "Title5", postContent: "This is Content"},
+			{title: "Title6", postContent: "This is Content"},
+			{title: "Title7", postContent: "This is Content"},
+			{title: "Title8", postContent: "This is Content"},
+			{title: "Title9", postContent: "This is Content"},
+			{title: "Title10", postContent: "This is Content"},
+			{title: "Title11", postContent: "This is Content"},
+			{title: "Title12", postContent: "This is Content"},
+			{title: "Title13", postContent: "This is Content"},
+			{title: "Title14", postContent: "This is Content"},
+			{title: "Title15", postContent: "This is Content"},
+			{title: "Title16", postContent: "This is Content"},
+			{title: "Title17", postContent: "This is Content"},
+			{title: "Title18", postContent: "This is Content"},
+			{title: "Title", postContent: "This is Content"},
+			{title: "Title20", postContent: "This is Content"},
+			{title: "Title", postContent: "This is Content"},
+			{title: "Title", postContent: "This is Content"},
+			{title: "Title", postContent: "This is Content"},
+			{title: "Title", postContent: "This is Content"},
+			{title: "Title25", postContent: "This is Content"},
+			{title: "Title", postContent: "This is Content"},
+			{title: "Title", postContent: "This is Content"},
+			{title: "Title", postContent: "This is Content"},
+			{title: "Title", postContent: "This is Content"},
+			{title: "Title30", postContent: "This is Content"},
+			{title: "Title", postContent: "This is Content"},
+			{title: "Title", postContent: "This is Content"},
+			{title: "Title", postContent: "This is Content"},
+			{title: "Title", postContent: "This is Content"},
+			{title: "Title35", postContent: "This is Content"},
+			{title: "Title", postContent: "This is Content"},
+			{title: "Title", postContent: "This is Content"},
+			{title: "Title", postContent: "This is Content"},
+			{title: "Title", postContent: "This is Content"},
+			{title: "Title40", postContent: "This is Content"},
+			{title: "Title", postContent: "This is Content"},
+			{title: "Title", postContent: "This is Content"},
+			{title: "Title", postContent: "This is Content"},
+			{title: "Title", postContent: "This is Content"},
+			{title: "Title", postContent: "This is Content"},
+			{title: "Title", postContent: "This is Content"},
+			{title: "Title", postContent: "This is Content"},
+			{title: "Title", postContent: "This is Content"},
+			{title: "Title", postContent: "This is Content"},
+			{title: "Title", postContent: "This is Content"},
+			{title: "Title", postContent: "This is Content"},
+			{title: "Title", postContent: "This is Content"},
+			{title: "Title", postContent: "This is Content"},
+			{title: "Title", postContent: "This is Content"},
+			{title: "Title", postContent: "This is Content"},
+			{title: "Title", postContent: "This is Content"},
+			{title: "Title", postContent: "This is Content"},
+			{title: "Title", postContent: "This is Content"},
+			{title: "Title", postContent: "This is Content"},
+			{title: "Title", postContent: "This is Content"},
+			{title: "Title", postContent: "This is Content"},
+			{title: "Title", postContent: "This is Content"}
+		];
+		// 총 게시글 수
+		const totalCount: number = postList.length;
+		// 보여질 게시글 개수 (10개씩 보기, 20개씩 보기)
+		const perPage: number = 10;
 
-    let paginationCount: number = Math.ceil(totalCount / perPage);
+		// METHODS
+		function checkCurrentPage(item): void {
+			console.log('emit item: ', item)
+		}
 
-    // Created: paginationList에 paginationCount 만큼 배열로 담는다.
-    for (let i: number = 1; i <= paginationCount; i++) {
-      paginationList.value.push(i as never);
-    }
-
-    return {
-      postList,
-      totalCount,
-      perPage,
-      paginationCount,
-      paginationList
-    };
-  }
+		return {
+			postList,
+			totalCount,
+			perPage,
+			checkCurrentPage
+		};
+	}
 });
 </script>
 <style>
 html {
-  width: 100%;
-  height: 100vh;
-  display: flex;
-  align-items: center;
-  justify-content: center;
+	width: 100%;
+	height: 100vh;
+	display: flex;
+	justify-content: center;
 }
 
 html,
@@ -122,22 +115,26 @@ span,
 ol,
 ul,
 li {
-  margin: 0;
-  padding: 0;
-  border: 0;
-  font-size: 100%;
-  font: inherit;
-  vertical-align: baseline;
+	margin: 0;
+	padding: 0;
+	border: 0;
+	font-size: 100%;
+	font: inherit;
+	vertical-align: baseline;
 }
 
 h1 {
-  display: flex;
-  align-items: center;
-  justify-content: center;
+	display: flex;
+	align-items: center;
+	justify-content: center;
 }
 
 ol,
 ul {
-  list-style: none;
+	list-style: none;
+}
+
+.group{
+	margin-top: 120px;
 }
 </style>
